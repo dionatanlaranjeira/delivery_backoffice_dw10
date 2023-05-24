aef2866ea888ba8ffce8cbb21422852597d4797d
dionatanlaranjeira <dionatanribeiro.netrr@gmail.com>
2023-05-24 12:09:22 -0400

    themes

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../../core/env/env.dart';
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
-import '../../core/ui/helpers/size_extensions.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -30,10 +27,28 @@ class _HomePageState extends State<HomePage> with Loader, Messages {
       ),
       body: Container(
         child: Container(
-          color: Colors.red,
-          width: context.percentWidth(.5),
-          height: context.percentHeight(.9),
-          child: Text(context.screenWidth.toString()),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: TextFormField(
+                    decoration: const InputDecoration(label: Text('Login')),
+                    validator: (String) => 'Erro',
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Botão'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
